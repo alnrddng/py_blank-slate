5b7a81c758a600fc06ccb3f54e63184d0d6cf817
Alan Redding <alan.e.redding@pm.me>
2023-04-11 20:09:33 -0600

    added the base template

---
+++ main.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Name: Basic Template
+Purpose: A simple template for me to use when building a new program.
+Version: 1.0
+License: MIT
+
+Developer(s): Alan Redding (GitHub: alnrddng)
+'''
+
+def main():
+    ''' function purpose '''
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
